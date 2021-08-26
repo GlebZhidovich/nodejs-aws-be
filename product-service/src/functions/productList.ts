6ae628ec06ddcd79bf40a747b787e5f5e3631093
GlebZhidovich <5925520@gmail.com>
2021-08-26 10:06:27 +0300

    refactor: rename id for product list

--- product-service/src/functions/productList.ts
+++ product-service/src/functions/productList.ts
@@ -32,7 +32,7 @@ export const productList: Product[] = [
   {
     "count": 7,
     "description": "Short Product Description2",
-    "id": "7567ec4b-b10c-48c5-9345-fc73c48a80a",
+    "id": "7567ec4b-b10c-48c5-9345-fc73c48a80ag",
     "price": 23,
     "title": "Product"
   },
