d08e18b22a88f56f8077d36d7cc0bfc07438801d
GlebZhidovich <gzhidovich@gmail.com>
2021-09-07 09:58:36 +0300

    feat: add sql script

---
+++ product-service/lesson4.sql
@@ -0,0 +1,24 @@
+insert into products (title, description, price)
+values 
+('JavaScript: The Definitive Guide, 7th Edition', 'JavaScript is the programming language of the web and is used by more software developers today than any other programming language.', 40), 
+('JavaScript Essentials', 'JavaScript is a key component of the modern web. Elisabeth Robson�s picks help you build a solid understanding of JS and apply it to your projects.', 30),
+('JavaScript Cookbook, 3rd Edition', 'You�ll get adaptable code samples that you can add to almost any project--and you�ll learn more about JavaScript in the process.', 25),
+('ProductTitle', 'Exploit the Power of Modern JavaScript and Avoid the Pitfalls JavaScript was originally designed for small-scale programming in web browsers, but modern JavaScript is radically different.', 25),
+('Learn JavaScript', 'Over the past few years, it�s become possible to do pretty much anything using only JavaScript: We can write entire front-end applications using React, Angular, or Vue.', 53), 
+('The Joy of JavaScript', 'The Joy of JavaScript introduces techniques that turn JavaScript programmers into JavaScript pros.', 45), 
+('Multithreaded JavaScript', 'Multithreaded JavaScript explores the various features that JavaScript runtimes have at their disposal for implementing multithreaded programming, providing both practical real-world examples, as well as reference material.', 43),
+('Eloquent JavaScript, 3rd Edition', 'This much anticipated and thoroughly revised third edition of Eloquent JavaScript dives deep into the JavaScript language to show you how to write beautiful, effective code.', 45),
+
+insert into stocks (product_id, count) 
+values 
+('2a94f5d9-5ccf-41c0-bbe6-fc057b422c61', 20), 
+('c7a264b5-123e-419f-9582-21311630c9fa', 13), 
+('4a87395e-f596-466e-9e9c-82ac8f2c0806', 15), 
+('e9158073-4cbf-4838-a433-bb4035114d00', 15), 
+('383f3842-df4e-4afa-b4be-71f9b8f574c4', 16), 
+('7a0fb8fd-d8ca-4a62-93b9-9b13fb3e4fca', 18), 
+('51d8b2e6-b6e6-46fc-980a-ceebad491f23', 20), 
+('e86ccd7b-8dfc-4830-bb1a-3b626b1fbfb2', 12), 
+('7913296a-55a5-41c8-a192-4ec8518b0c81', 10), 
+('8eb80494-5304-43d5-9fb8-875c839e0acb', 10), 
+('02bc9dbe-b09e-4b41-9f2c-5be922509369', 10)
