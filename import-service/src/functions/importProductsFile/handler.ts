f030ea2d2e1578c3bbf517ca151bfbb4d9684644
GlebZhidovich <gzhidovich@gmail.com>
2021-09-16 22:09:47 +0300

    feat: add middleware

--- import-service/src/functions/importProductsFile/handler.ts
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -1,37 +1,33 @@
-import 'source-map-support/register';
-
+import {
+  PutObjectCommand,
+  PutObjectCommandInput,
+  S3Client,
+  S3ClientConfig,
+} from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
-import { GetObjectCommand, GetObjectCommandInput, S3Client, S3ClientConfig } from '@aws-sdk/client-s3';
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import 'source-map-support/register';
 
 const BUCKET_NAME = 'import-service-bucket-app';
 
-const importProductsFile: ValidatedEventAPIGatewayProxyEvent<object> = async ({ pathParameters }) => {
-
-  try {
-    const { fileName } = pathParameters;
-    const clientParams: S3ClientConfig = {
-      region: "eu-west-1"
-    }
-    const getObjectParams: GetObjectCommandInput = {
-      Bucket: BUCKET_NAME,
-      Key: `uploaded/${fileName}.csv`,
-    }
-    const client = new S3Client(clientParams);
-    const command = new GetObjectCommand(getObjectParams);
-    const url = await getSignedUrl(client, command, { expiresIn: 600 });
-
-    return formatJSONResponse({
-      url,
-    });
-  } catch (err) {
-    return formatJSONResponse({
-      error: err.message,
-    }, 500);
-  }
-
-}
+const importProductsFile = async ({
+  pathParameters,
+}): Promise<{ url: string }> => {
+  const { filename } = pathParameters;
+  const clientParams: S3ClientConfig = {
+    region: 'eu-west-1',
+  };
+  const getObjectParams: PutObjectCommandInput = {
+    Bucket: BUCKET_NAME,
+    Key: `uploaded/${filename}`,
+    ContentType: 'text/csv',
+  };
+  const client = new S3Client(clientParams);
+  const command = new PutObjectCommand(getObjectParams);
+  const url = await getSignedUrl(client, command, { expiresIn: 60 });
+  return { url };
+};
 
 export const main = middyfy(importProductsFile);
--- import-service/src/functions/importProductsFile/index.ts
+++ import-service/src/functions/importProductsFile/index.ts
@@ -6,7 +6,8 @@ export default {
     {
       http: {
         method: 'get',
-        path: 'import/{fileName}',
+        path: 'import/{filename}',
+        cors: true,
       }
     }
   ]
